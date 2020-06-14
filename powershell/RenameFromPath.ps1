9214e53c7abbb7c259132a8c1d01423574d0a430
leoreh <45538676+leoreh@users.noreply.github.com>
2020-06-14 07:18:19 +0300

    Update RenameFromPath.ps1

--- powershell/RenameFromPath.ps1
+++ powershell/RenameFromPath.ps1
@@ -1,11 +1,14 @@
-﻿$basepath = 'H:\Data\Dat\lh50\lh50'
+﻿$basepath = 'F:\Daniel IIS 11.6.20\IIS\APPKi\New analysis'
 cd($basepath) # path to files
 
 # rename by adding path name
 #Get-ChildItem $basepath -Filter *.wcp -Recurse | Rename-Item -NewName { $_.Directory.Name+'.abf'}
 
 # rename by replacing one string with another
-Get-ChildItem $basepath | Rename-Item -NewName {$_.name -replace '_','_200323'}
+Get-ChildItem $basepath | Rename-Item -NewName {$_.name -replace '.lfp.bs','.bs'}
+Get-ChildItem $basepath | Rename-Item -NewName {$_.name -replace '.lfp.ep','.ep'}
+Get-ChildItem $basepath | Rename-Item -NewName {$_.name -replace '.lfp.iis','.iis'}
+Get-ChildItem $basepath | Rename-Item -NewName {$_.name -replace '.lfp.lfp','.lfp'}
 
 # rename by adding string to filename
 # Get-ChildItem $basepath | Rename-Item -NewName {"lh49_200319" + $_.name}
